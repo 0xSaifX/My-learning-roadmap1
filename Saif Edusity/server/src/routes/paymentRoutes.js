1e9021e77419acbd6445f56ecf6f5020a1f5d958
0xSaifX <saifuddeen.tech@gmail.com>
2026-01-20 18:15:16 +0100

    delete all files/folders

--- Saif Edusity/server/src/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createCheckoutSession } = require('../controllers/paymentController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/create-checkout-session', protect, createCheckoutSession);
-
-module.exports = router;
